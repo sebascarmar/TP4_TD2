a8520b78b4ec4cff65e148a8b057bc0957ea7599
sebacarmar <sebacarmar@gmail.com>
2022-10-08 18:44:29 -0300

    Agrega código inicial

---
+++ 3_ControlDeAccesoConModoNoCanonico.c
@@ -0,0 +1,77 @@
+#include <stdio.h>
+#include <termios.h>
+
+#define FD_STDIN 0
+#define LENGTH_PSSW 5
+
+int main( void )
+{
+  struct termios t_old, t_new;                              // Estructuras para atributos del teclado.
+  char contra[LENGTH_PSSW+1] = {'8','3','3','8','A','\0'};  // Contraseña de LENGTH_PSSW carcateres (más el caracter NULL).
+  char password[LENGTH_PSSW+1] = {'\0'};                    // Buffer para almacenar la contraseña que se ingresa.
+  char tecla;                                               // Variable que almacena cada caracter que se ingresa.
+  int i, asteriscosImpresos, contraCorrecta;     
+
+
+  /*******************************SETEO DEL MODO NO CANÓNICO*****************************************/
+  tcgetattr(FD_STDIN, &t_old);        // Lee atributos del teclado.
+  t_new = t_old;
+  t_new.c_lflag &= ~(ECHO | ICANON);  // Anula entrada canónica y eco.
+  tcsetattr(FD_STDIN,TCSANOW,&t_new); // Actualiza con los valores nuevos de la config (TCSANOW = activa la modificación inmediatamente).
+
+  
+  /**********************INGRESO DE CONTRASEÑA  Y PROCESAMIENTO DE DATOS****************************/
+  for(int numIntentos = 0 ; numIntentos < 3; numIntentos++)
+  {
+    tecla = 0;              // Almacena cada caracter que se ingresa.
+    i = 0;                  // Índice para guardar los caracteres en el buffer 'password'.
+    asteriscosImpresos = 0; // Cuenta los ast. que se imprimen, para poder borrar todos si se quiere.
+    contraCorrecta = 0;     // Al recorrer 'password' y comparar con 'contra', si vale LENGTH_PSSW entonces es válida la passwd ingresada.
+
+    printf(" Ingrese su password de 5 dígitos: ");
+    while ( tecla!=10 ){    // Ingresar hasta oprimir enter. Parte de la simulacion del modo canónico.
+        tecla = getchar();
+        
+        if( tecla!=127 &&  tecla!=10 ) // Si no se presionan DEL ni ENTER, es verdadero.
+        {
+          if( i<=(LENGTH_PSSW-1) && i==asteriscosImpresos)// 'i' no puede ser mayor al largo de la contra, y debe coincidir con los asteriscos 
+          {                                               //en pantalla.
+            password[i] = tecla;
+            i++;
+          }                   
+          
+          printf("*");
+          asteriscosImpresos++;
+          
+        }else                          // Si se presionan DEL o ENTER, es verdadero
+        {
+          if( asteriscosImpresos>0 ) // Solo borra caracteres en pantalla si fueron asteriscos impresos.
+          {
+            printf("\b \b"); // Imprime un backspace, sobreescribe con un espacio, y vuelve el cursor hacia atrás con otro backspace.
+            if( i>0 )
+              i--;       
+            asteriscosImpresos--;
+          }
+        }
+        
+    }
+    
+    for(int j = 0 ; j<LENGTH_PSSW ; j++) // Analiza si la contraseña ingresada es correcta.
+    { 
+      if( contra[j] == password[j] )
+        contraCorrecta++;
+    }
+    
+    if( contraCorrecta == 5 ){
+      printf("\n Bienvenido al sistema\n");
+      numIntentos = 3; 
+    }else
+      printf("\n Contraseña incorrecta\n");
+    
+  }
+
+  /*********************************SETEO DEL MODO CANÓNICO*******************************************/
+  tcsetattr(FD_STDIN, TCSANOW, &t_old); // Actualiza los atributos del teclado con los valores previos.
+
+  return 0;
+}
