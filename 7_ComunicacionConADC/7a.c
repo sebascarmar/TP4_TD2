021a7849c74697b803a3ba96bec2fa19e4b4c327
sebascarmar <sebascarmar@gmail.com>
2022-10-15 22:36:12 -0300

    Agrega mejoras y da formato a los comentarios

--- 7_ComunicacionConADC/7a.c
+++ 7_ComunicacionConADC/7a.c
@@ -3,49 +3,58 @@
 #include <stdlib.h>
 #include <stdio.h>
 
-
-/*************************ANCHO PARA EL INFORME************************************/
-
-
+/***********************Direcciones de registros***********************************/
 #define ADDRESS 0x48
 #define A0 ADDRESS+0
 #define A1 ADDRESS+1
 #define A2 ADDRESS+2
 #define A3 ADDRESS+3
-#define A4 ADDRESS+4
 
 
-int main()   
+int main( int argc, char *argv[] )   
 {
-	//const char PCF8591 = 0x48; // direccion base
-	
-	int fd, adcVal;	//declaracion de variables para acceder al adc
+  int fd, valorADC;	// Declaracion de variables para acceder al ADC.
+  
+/****************************Mapeo de pines****************************************/
+  if ( wiringPiSetup() == -1 ) // Inicializa los pines siguiendo el esquema de WiringPi.
+  {
+    printf("Error al mapear el GPIO.\n");
+    exit(EXIT_FAILURE);	
+  }
+  
+  fd = wiringPiI2CSetup(ADDRESS); // Inicializa el sistema I2C con el ID 
+                                 //del dispositivo.
+  if( fd <= -1 )
+  {
+    printf("Error al inicializar el systema I2C.\n");
+    exit(EXIT_FAILURE);	
+  }
 
-	if (wiringPiSetup () == -1) exit (1);	//
-
-	fd = wiringPiI2CSetup(ADDRESS);	
-	// configuracion del bus I2C, inicializa el sistema I2C con el ID del dispositivo.
-
-	while (1)   {
-	//read ADC0
-	wiringPiI2CReadReg8(fd, A4); //primer lectura 
-	adcVal = wiringPiI2CReadReg8(fd, A4);//segunda lectura valida
-	printf("Pot value = %d \n", adcVal);
-	
-	//read ADC1
-	wiringPiI2CReadReg8(fd, A1); //primer lectura
-	adcVal = wiringPiI2CReadReg8(fd, A1);
-	printf("Photocell = %d \n", adcVal);
-	
-	//read ADC2
-	wiringPiI2CReadReg8(fd, A2); //primer lectura
-	adcVal = wiringPiI2CReadReg8(fd, A2);
-	printf("Termister = %d \n\n", adcVal);
+/****************Lectura de los valores del ADC mediante I2C************************/
+  while( 1 )
+  {
+    // Lectura del canal ADC0 (potenciómetro).
+    wiringPiI2CReadReg8(fd, A0); // Indica el registro a leer.
+    valorADC = wiringPiI2CReadReg8(fd, A0);// Lectura del valor.
+    printf("Voltaje en el potenciómetro = %.3f V\n", (float) valorADC*3.3/255);
+    
+    // Lectura del canal ADC1 (fotoresistor).
+    wiringPiI2CReadReg8(fd, A1); // Indica el registro a leer.
+    valorADC = wiringPiI2CReadReg8(fd, A1); // Lectura del valor.
+    printf("Voltaje en el fotoresistor  = %.3f V\n", (float) valorADC*3.3/255);
+    
+    // Lectura del canal ADC2 (termistor).
+    wiringPiI2CReadReg8(fd, A2); // Indica el registro a leer.
+    valorADC = wiringPiI2CReadReg8(fd, A2); // Lectura del valor.
+    printf("Voltaje en el termistor     = %.3f V\n", (float) valorADC*3.3/255);
+    		  
+    // Lectura del canal ADC3 (puesto a GND).
+    wiringPiI2CReadReg8(fd, A3); // Indica el registro a leer.
+    valorADC = wiringPiI2CReadReg8(fd, A3); // Lectura del valor.
+    printf("Voltaje en el cuarto canal  = %.3f V\n\n", (float) valorADC*3.3/255);
+    
+    delay(500);
+  }		  
 			  
-	delay(500);
-			  
-     }
-			  
-return 0;
-			  
+  return 0;
 }
