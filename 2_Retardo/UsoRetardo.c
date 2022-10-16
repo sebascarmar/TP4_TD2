2f3dc61339b8fad0d469927bb60439fc145009bf
sebascarmar <sebascarmar@gmail.com>
2022-10-16 00:04:50 -0300

    Agrega mejora innecesaria pero muy bacana

--- 2_Retardo/UsoRetardo.c
+++ 2_Retardo/UsoRetardo.c
@@ -1,29 +1,35 @@
 #include <stdio.h>
 
+#define FD_STDOUT 1
 extern void retardo(unsigned long int a);
 
 int main( int argc, char *argv[] )
 {
-  long unsigned int numRetardo=900000000;
-
+  long unsigned int numRetardo=500000000;
 
   retardo(numRetardo);
-  printf("Esto es");
+  dprintf(FD_STDOUT, "Esto es ");
   retardo(numRetardo);
 
-  printf(" \n un mensaje \n");
+  dprintf(FD_STDOUT, "un mensaje ");
   retardo(numRetardo);
 
-  printf(" progresivo.\n");
+  dprintf(FD_STDOUT, "progresivo");
   retardo(numRetardo);
 
-  printf("Se comporta rara la primer línea.\n");
-  retardo(numRetardo);
+  for( int i=1 ; i <= 6 ; i++ ) // Puntos suspensivos.
+  {
+    dprintf(FD_STDOUT, ".");
+    retardo(numRetardo);
+  
+    if( i%3==0 )
+    {
+      dprintf(FD_STDOUT, "\b\b\b   \b\b\b");
+      retardo(numRetardo);
+    }
+  }
 
-  printf("Adiós.\n");
-  retardo(numRetardo);
-
-
+  dprintf(FD_STDOUT, ".\nAdiós.\n");
   return 0;
 }
 
