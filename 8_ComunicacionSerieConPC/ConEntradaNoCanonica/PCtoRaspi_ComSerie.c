48e901cc557afc2792ca31a57d7888fc18278903
sebacarmar <sebacarmar@gmail.com>
2023-01-15 23:43:25 -0300

    Elimina borradores

--- 8_ComunicacionSerieConPC/ConEntradaNoCanonica/PCtoRaspi_ComSerie.c
+++ 8_ComunicacionSerieConPC/ConEntradaNoCanonica/PCtoRaspi_ComSerie.c
@@ -34,58 +34,61 @@ int main( int argc, char *argv[] )
 
 
 
-  /*********************Seteo del modo NO canónico NO bloqueante********************/
-  tcgetattr( fdUART, &t_oldUART );        // Lee atributos del teclado.
-  t_newUART = t_oldUART;                        // Guarda los atributos originales.
-	//A PARTIR DE AQUÍ SE COMIENZAN A CONFIGURAR LOS NUEVOS ATRIBUTOS
-	t_newUART = t_oldUART ;//asigno al puntero tt_new la misma dirección que ttyold ¡¡¡¡¡CONSULTAR!!!!
+  /************Seteo del modo NO canónico NO bloqueante en el PUERTO SERIAL**********/
+  tcgetattr( fdUART, &t_oldUART );        // Lee atributos de la UART.
+  t_newUART = t_oldUART;                  // Guarda los atributos originales.
 	
-	cfsetospeed( &t_newUART, 9600 ) ; //velocidad de comunicación de salida
-	cfsetispeed( &t_newUART, 9600 ) ; //velocidad de comunicación de entrada
-	
-	t_newUART.c_cflag = ( t_newUART.c_cflag & ~CSIZE ) | CS8 ; // 8 bits de datos (8)
-	t_newUART.c_cflag &= ~( PARENB | PARODD ) ;	      // sin paridad     (N)
-	t_newUART.c_cflag &= ~CSTOPB ;			      // 1 bit de stop   (1)
-		t_newUART.c_iflag |= IGNCR ;/* SE CONFIGURA QUE SE IGNORE EL CARACTER '\r' EN LA ENTRADA.*/
-		 //t_newUART.c_oflag = 0 ;	 		 //no remapping , no delays
+  //A PARTIR DE AQUÍ SE COMIENZAN A CONFIGURAR LOS NUEVOS ATRIBUTOS
+  cfsetospeed( &t_newUART, 9600 ); // Velocidad de comunicación de salida.
+  cfsetispeed( &t_newUART, 9600 ); // Velocidad de comunicación de entrada.
+  
+  t_newUART.c_cflag = ( t_newUART.c_cflag & ~CSIZE ) | CS8; // 8 bits de datos (8)
+  t_newUART.c_cflag &= ~( PARENB | PARODD );	            // sin paridad     (N)
+  t_newUART.c_cflag &= ~CSTOPB;			            // 1 bit de stop   (1)
+  t_newUART.c_iflag |= IGNCR;  // Ignora el caracter '\r' en la entrada.
 	
   t_newUART.c_lflag &= ~(ECHO | ICANON);  // Anula entrada canónica y eco.
   t_newUART.c_cc[VMIN] = 0;               // No espera que ingrese ningún caracter.
   t_newUART.c_cc[VTIME] = 0;              // Tampoco espera ningún tiempo.
 
-  //tcsetattr( fdUART,TCSANOW,&t_newUART ); // Setea los valores nuevos de la config.
-  	if( tcsetattr( fdUART, TCSAFLUSH, &t_newUART) != 0 ) 
-	{
-		printf( "ERROR: tcsetatrr\n" ) ;
-		return -1 ;
-	}
-
-  /****Bucle  de ESCRITURA en la UART (termina cuando se aprieta una tecla)****/
-  while( buf[0] != '!'  )
-  {     
-        detectaTecla = read( FD_STDIN, buf, 1 ); // read() lee 1 caracter de la ENTRADA
-                                                 //ESTANDAR y lo almacena en buf[0]. Además,
-                                                 //retorna la cantidad de caracteres que lee.
-        //dprintf(1, "%c", buf[0]); // Imprime el carac. ingresado, simula entrada canónica.
-        write( 1, buf, 1  ); // Escribe 1 byte del contenido de cadena al "fdUART".
-        write( fdUART, buf, 1  ); // Escribe 1 byte del contenido de cadena al "fdUART".
-        tcdrain( fdUART ); // Espera a que lo que se haya escrito en "fdUART" se transmita.
-        tcflush(fdUART, TCIOFLUSH);
-        if(buf[0] != '!') // Evita que se imprima el mismo caracter indefinidamente.
-            buf[0]='\0';  
+  if( tcsetattr( fdUART, TCSAFLUSH, &t_newUART) != 0 ) // Setea las configuraciones.
+  {
+    printf( "ERROR: tcsetatrr\n" ) ;
+    return -1 ;
   }
 
-  sleep(3);
+  /****Bucle  de ESCRITURA en la UART (termina cuando se aprieta una tecla)****/
+  printf(" --------------------------------------\n");
+  printf(" - Com. serie (termina al enviar '!') -\n");
+  printf(" --------------------------------------\n");
+  while( buf[0] != '!'  )
+  {     
+    read( FD_STDIN, buf, 1 ); // read() lee 1 caracter de la ENTRADA ESTANDAR
+                              //y lo almacena en buf[0].
+    //dprintf(1, "%c", buf[0]); 
+    write( 1, buf, 1  ); // Imprime el caracter ingresado, simula entrada canónica
+   
+    write( fdUART, buf, 1  ); // Escribe 1 byte del contenido de cadena al "fdUART".
+    tcdrain( fdUART ); // Espera a que lo que se haya escrito en "fdUART" se transmita.
+    tcflush(fdUART, TCIOFLUSH);
+    
+    if(buf[0] != '!') // Evita que se imprima el mismo caracter indefinidamente.
+        buf[0]='\0';  
+  }
+
   /***LECTURA de la cadena en la UART e impresión en pantalla de lo recibido***/
   write(1,"\n", 1);
   while( buf[0] != '\0' )
   {
-        detectaTecla = read( fdUART, buf, 1 ); // read() lee 1 caracter del PUERTO
-                                                //SERIAL y lo almacena en buf[0]. Además,
-                                                //retorna la cantidad de caracteres que lee.
-        write( 1, buf, 1  ); // Escribe 1 byte del contenido de cadena al "fdUART".
+    read( fdUART, buf, 1 ); // read() lee 1 caracter del PUERTO SERIAL y lo
+                            //almacena en buf[0].
+    if(buf[0] != '!')
+      //dprintf(1, "%c", buf[0]);
+      write( 1, buf, 1  ); // Imprime 1 byte del contenido de la cadena.
+    if(buf[0] != '\0') // Evita que se imprima el mismo caracter indefinidamente.
+      buf[0]='!';  
   }
- printf("\n");
+  printf("\n");
 
 
   /************************Seteo del modo canónico*****************************/
